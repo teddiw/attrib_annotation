ded76967d990dd56cdd0493c3c66c31d153c12a4
Theodora Worledge <theodoraworledge@gmail.com>
2024-05-03 00:29:12 -0700

    edit2

--- sl_app.py
+++ sl_app.py
@@ -62,36 +62,35 @@ if (st.session_state["username"]):
              }).execute()
         touched_response_ids = []
 
-    # # get instances that still need annotation
-    # remaining_response_ids = pd.DataFrame(db_conn.table("instances_to_annotate").select("*").execute().data)
-    # remaining_response_ids = remaining_response_ids.sort_values(by='response_id', ascending=True)
-    # viable_response_ids = remaining_response_ids[~remaining_response_ids['response_id'].isin(touched_response_ids)]
-    # hit_response_ids_df = viable_response_ids.iloc[:min(len(viable_response_ids), 10)]
+    # get instances that still need annotation
+    remaining_response_ids = pd.DataFrame(db_conn.table("instances_to_annotate").select("*").execute().data)
+    remaining_response_ids = remaining_response_ids.sort_values(by='response_id', ascending=True)
+    viable_response_ids = remaining_response_ids[~remaining_response_ids['response_id'].isin(touched_response_ids)]
+    hit_response_ids_df = viable_response_ids.iloc[:min(len(viable_response_ids), 10)]
     
-    # # identify the instances for this hit
-    # st.session_state["hit_response_ids"] = hit_response_ids_df['response_id'].tolist()
-    # hit_op_ls = []
-    # for i in range(len(hit_response_ids_df)):
-    #     remaining_ops = hit_response_ids_df.iloc[i]['ops']
-    #     sampled_op = random.sample(remaining_ops, 1)[0]
-    #     hit_op_ls.append(sampled_op)
-    #     response_id = hit_response_ids_df.iloc[i]['response_id']
-    #     # remove op from instances_to_annotate
-    #     if (len(remaining_ops) == 1):
-    #         # remove row from instances_to_annotate
-    #         db_conn.table('instances_to_annotate').delete().eq('response_id', response_id).execute()
-    #     else:
-    #         remaining_ops.remove(sampled_op) 
-    #         db_conn.table('instances_to_annotate').update({'ops': remaining_ops}).eq('response_id', response_id).execute()
-    # st.session_state["hit_ops"] = hit_op_ls
+    # identify the instances for this hit
+    st.session_state["hit_response_ids"] = hit_response_ids_df['response_id'].tolist()
+    hit_op_ls = []
+    for i in range(len(hit_response_ids_df)):
+        remaining_ops = hit_response_ids_df.iloc[i]['ops']
+        sampled_op = random.sample(remaining_ops, 1)[0]
+        hit_op_ls.append(sampled_op)
+        response_id = hit_response_ids_df.iloc[i]['response_id']
+        # remove op from instances_to_annotate
+        if (len(remaining_ops) == 1):
+            # remove row from instances_to_annotate
+            db_conn.table('instances_to_annotate').delete().eq('response_id', response_id).execute()
+        else:
+            remaining_ops.remove(sampled_op) 
+            db_conn.table('instances_to_annotate').update({'ops': remaining_ops}).eq('response_id', response_id).execute()
+    st.session_state["hit_ops"] = hit_op_ls
 
-    # # form the dataframe of instance info for this hit
-    # rows_to_annotate = []
-    # for response_id, op in zip(st.session_state["hit_response_ids"], st.session_state["hit_ops"]):
-    #     rows_to_annotate.append(df[(df['ID']==response_id)&(df['op']==op)])
-    # hit_df = pd.concat(rows_to_annotate, ignore_index=True) # yay :D
-    # st.session_state["hit_df"] = hit_df
-    breakpoint()
+    # form the dataframe of instance info for this hit
+    rows_to_annotate = []
+    for response_id, op in zip(st.session_state["hit_response_ids"], st.session_state["hit_ops"]):
+        rows_to_annotate.append(df[(df['ID']==response_id)&(df['op']==op)])
+    hit_df = pd.concat(rows_to_annotate, ignore_index=True) # yay :D
+    st.session_state["hit_df"] = hit_df
     st.switch_page('pages/response_level.py')
   
 
