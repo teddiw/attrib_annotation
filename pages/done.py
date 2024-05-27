4bc0d9b76932f113d7e76918558b7426a565cacb
Theodora Worledge <theodoraworledge@gmail.com>
2024-05-27 10:14:36 -0700

    fixed completion coes

--- pages/done.py
+++ pages/done.py
@@ -19,11 +19,12 @@ def generate_completion_code():
     alphanumeric = string.ascii_letters + string.digits
     return ''.join(random.choice(alphanumeric) for _ in range(25))
 
-completion_code = generate_completion_code(20)
+completion_code = generate_completion_code()
 st.session_state["db_conn"].table('hit_completion_codes').insert({
                                              "completion_code": completion_code, 
                                              "annotator_id": st.session_state["username"],
-                                             "used_code": 0,
+                                             "code_used": False
                                              }).execute()
-print(completion_code)
 st.markdown('''# Done! Thank you so much! :raised_hands:''')
+st.markdown('''Please enter the completion code below on the MTurk HIT webpage for compensation.''')
+st.markdown(completion_code)
