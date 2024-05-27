c64c081adfde15d798382fd907df4a87bc28cbcd
Theodora Worledge <theodoraworledge@gmail.com>
2024-05-27 09:55:40 -0700

    added completion code

--- pages/done.py
+++ pages/done.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import random
+import string
 
 ## Page configs
 st.set_page_config(initial_sidebar_state="collapsed")
@@ -13,4 +15,15 @@ st.markdown(
 """,
     unsafe_allow_html=True,
 )
+def generate_completion_code():
+    alphanumeric = string.ascii_letters + string.digits
+    return ''.join(random.choice(alphanumeric) for _ in range(25))
+
+completion_code = generate_completion_code(20)
+st.session_state["db_conn"].table('hit_completion_codes').insert({
+                                             "completion_code": completion_code, 
+                                             "annotator_id": st.session_state["username"],
+                                             "used_code": 0,
+                                             }).execute()
+print(completion_code)
 st.markdown('''# Done! Thank you so much! :raised_hands:''')
