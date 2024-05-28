ed5d40a88a8656faef1668dec0cc0718ccfaad2b
Theodora Worledge <theodoraworledge@gmail.com>
2024-05-28 12:26:54 -0700

    mturk live

--- pages/done.py
+++ pages/done.py
@@ -19,16 +19,17 @@ st.markdown(
 
 # create mturk client
 MTURK_SANDBOX = 'https://mturk-requester-sandbox.us-east-1.amazonaws.com'
+MTURK = 'https://mturk-requester.us-east-1.amazonaws.com'
 mturk = boto3.client('mturk',
                     aws_access_key_id = st.secrets["aws_access_key_id"],
                     aws_secret_access_key = st.secrets["aws_secret_access_key"],
                     region_name='us-east-1',
-                    endpoint_url = MTURK_SANDBOX
+                    endpoint_url = MTURK
                     )
 # get new number of completed HITs for this group
 num_worker_annotations = len(st.session_state["db_conn"].table(st.session_state['annotations_db']).select("query_id").eq("annotator_id", st.session_state["username"]).execute().data)//5
 # update the worker's qualification
-NUM_TRIALS_QUAL_ID = '3O42DSCJPU0X16ST9JDZLV1W82OX6J'
+NUM_TRIALS_QUAL_ID = '3LW1BECZ1OKIR84E7XGS2E027J5RS4' # SB '3O42DSCJPU0X16ST9JDZLV1W82OX6J' 
 worker_id = st.session_state["username"].split('_')[0]
 response = mturk.associate_qualification_with_worker(
             QualificationTypeId=NUM_TRIALS_QUAL_ID,
